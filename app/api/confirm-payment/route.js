72325f1f9c1ae4138b4ef994d080f2a6cd37b194
Azaz ali <aizaz.0938@gmail.com>
2025-03-26 10:19:17 +0500

    add security for booking

--- app/api/confirm-payment/route.js
+++ app/api/confirm-payment/route.js
@@ -1,9 +1,10 @@
-// import { NextResponse } from "next/server";
+// This file will be deleted because it was just for testing but not working properly.
+import { NextResponse } from "next/server";
 
-// export async function GET(req) {
-//   try {
-//      return NextResponse.json({ success: true, message: "Payment confirmed" });
-//   } catch (error) {
-//     return NextResponse.json({ success: false, error: error.message });
-//   }
-// }
+export async function GET(req) {
+  try {
+    return NextResponse.json({ success: true, message: "Payment confirmed" });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message });
+  }
+}
