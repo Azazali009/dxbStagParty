03be73617803e1ce2257af6eefed1e3e5cb0c1b0
Azaz ali <aizaz.0938@gmail.com>
2025-04-10 00:08:55 +0500

    add nextauth

--- app/bookings/page.js
+++ app/bookings/page.js
@@ -25,7 +25,7 @@ export default async function Page({ searchParams }) {
   return (
     <div className="mx-auto flex max-w-5xl flex-col gap-4 p-4 text-sm">
       <h1 className="mb-6 text-2xl font-semibold capitalize">
-        all booking "{bookings.length}"
+        all booking {bookings.length}
       </h1>
       <BookingFilter />
       <DisplayBookingTable bookings={filteredBookings} />
