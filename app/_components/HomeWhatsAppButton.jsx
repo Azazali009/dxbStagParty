f4011c187cec3f9a44f226e01ecea2e448e9fb96
Azaz ali <aizaz.0938@gmail.com>
2025-04-15 11:44:41 +0500

    fix whatsapp button icon path

--- app/_components/HomeWhatsAppButton.jsx
+++ app/_components/HomeWhatsAppButton.jsx
@@ -10,7 +10,7 @@ export default function HomeWhatsAppButton() {
         className="bg-navyBlue border-softGold/20 flex w-fit items-center gap-4 rounded-lg border px-6 py-3 duration-300 hover:scale-95 hover:shadow-xl"
       >
         <Image
-          src={"/images/wpicon.png"}
+          src={"/images/wpIcon.png"}
           width={25}
           height={25}
           alt="whatsapp"
