b4b7bdcc2f6342fad00c13a4fd1d35e9920ec3d9
Azaz ali <aizaz.0938@gmail.com>
2025-04-27 16:41:10 +0500

    add contact page and finish home page

--- app/_components/AdminHeader.jsx
+++ app/_components/AdminHeader.jsx
@@ -10,10 +10,10 @@ export default function AdminHeader() {
         className="h-12 w-2/3 rounded-full bg-primary px-4 outline-none duration-300 placeholder:text-sm placeholder:tracking-wider focus:outline-matalicGold"
       />
       <Link
-        className="flex items-center gap-3 rounded-md bg-black px-6 py-3 capitalize shadow-lg duration-500 hover:scale-95 hover:opacity-70 hover:shadow-none"
+        className="flex h-10 items-center gap-3 rounded-md bg-sky-600 px-6 capitalize shadow-lg duration-300 hover:bg-sky-800 hover:shadow-none"
         href={"/dashboard/create-activity"}
       >
-        <Image src={plusIcon} width={20} height={20} alt="add" />{" "}
+        <span className="text-xl">+</span>
         <span>create activity</span>
       </Link>
     </div>
--- app/dashboard/bookings/page.js
+++ app/dashboard/bookings/page.js
@@ -24,9 +24,7 @@ export default async function Page({ searchParams }) {
     );
   return (
     <div className="mx-auto flex max-w-5xl flex-col gap-4 p-4 text-sm">
-      <h1 className="mb-6 text-2xl font-semibold capitalize">
-        all booking {bookings.length}
-      </h1>
+      <h1 className="mb-6 text-2xl font-semibold capitalize">all booking</h1>
       <BookingFilter />
       <DisplayBookingTable bookings={filteredBookings} />
     </div>
