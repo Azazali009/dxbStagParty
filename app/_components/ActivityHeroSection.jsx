b2076f4bc605cd533c60e44e2e5e86bbb6fbfee9
Azaz ali <aizaz.0938@gmail.com>
2025-05-15 17:38:31 +0500

    add blog section to admin dashboard

--- app/_components/ActivityHeroSection.jsx
+++ app/_components/ActivityHeroSection.jsx
@@ -3,15 +3,13 @@ import AnimatedHeading from "./AnimatedHeading";
 
 export default function ActivityHeroSection() {
   return (
-    <div className="relative -mt-[110px] flex h-[800px] min-h-screen items-end gap-14 bg-[url('/images/activity-page-bg.webp')] bg-cover bg-no-repeat px-8 py-20">
-      {/* overlay */}
-      <div className="absolute left-0 top-0 flex h-full w-full items-end"></div>
-      <div className="relative z-10 max-w-[70%] space-y-6">
+    <div className="relative -mt-[170px] flex h-[800px] min-h-screen items-end gap-14 bg-[url('/images/activity-page-bg.webp')] bg-cover bg-no-repeat px-8 py-20">
+      <div className="relative z-10 mb-40 space-y-6">
         <AnimatedHeading
-          className={`text-balance pt-16 text-left text-xl !font-normal uppercase !leading-[1.1] text-softGold ${BebasNeue.className} sm:text-wrap md:text-6xl`}
+          className={`text-balance pt-16 text-left text-xl !font-normal uppercase !leading-[1.1] text-softGold ${BebasNeue.className} sm:text-wrap md:text-7xl`}
         >
           {" "}
-          Dubai most legendary stag party experiences
+          Dubai most legendary <br /> stag party experiences
         </AnimatedHeading>
         <p className="">60+ curated activities. One unforgettable weekend.</p>
       </div>
