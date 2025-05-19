4a5f8f07b0f889e580172d974249b85f646f967e
Azaz ali <aizaz.0938@gmail.com>
2025-05-19 05:22:00 +0500

    add blog section to admin dashboard

--- app/_components/BlogHero.jsx
+++ app/_components/BlogHero.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { playfairDisplay } from "../layout";
+import Link from "next/link";
 
 export default function BlogHero() {
   return (
     <div className="relative min-h-[800px] bg-[url(/images/blog-bg.webp)] bg-cover bg-no-repeat">
       {/* overlay */}
       <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-r from-navyBlue/70 to-transparent"></div>
-      <div className="relative z-10 flex min-h-[800px] w-[50%] flex-col justify-center gap-4 p-6">
-        <p className="uppercase">Guid</p>
+      <div className="relative z-10 flex min-h-[800px] w-[50%] flex-col justify-center gap-6 p-6">
+        <p className="uppercase">Guide</p>
         <h1
           className={`${playfairDisplay.className} text-8xl font-semibold capitalize leading-[1.3]`}
         >
@@ -16,6 +17,14 @@ export default function BlogHero() {
         <p className="text-xl">
           Party like ballers with exclusive table, lounges & more
         </p>
+        <div>
+          <Link
+            href="#"
+            className="rounded-lg border-2 border-[#70592f] bg-[#70592f] px-6 py-2 text-lg capitalize duration-300 hover:bg-reddish"
+          >
+            Read more
+          </Link>
+        </div>
       </div>
     </div>
   );
