ae4a0fe7eaf0f2774ba8338ad0ac3cda6c023f3c
Azaz ali <aizaz.0938@gmail.com>
2025-08-27 13:03:25 +0500

    add voting data in admin panel

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,6 +1,5 @@
 import ContactHero from "../_components/ContactHero";
 import ContactSection from "../_components/ContactSection";
-import HomeCTA from "../_components/PreFooter";
 import StagConfidenceSection from "../_components/StagConfidenceSection";
 export default function Page() {
   return (
